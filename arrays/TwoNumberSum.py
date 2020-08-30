6c9c0fabd35ba73e0ee594a8cc711addbe2ea71c
Aleksei Koltcov <alexeykolcov@gmail.com>
2020-08-30 23:46:33 +0200

    solve "Two Number Sum" problem

---
+++ arrays/TwoNumberSum.py
@@ -0,0 +1,27 @@
+# Two Number Sum
+#
+# Write a function that takes in a non-empty array of s=distinct integers and an integer
+# representing a target sum. If any two numbers in the input array sum up to the target sum,
+# the function should return them in an array, in any order. If no two numbers sum up to the target
+# sum, the function should return an empty array.
+#
+# Note that the target sum has to be obtained by summing two different integers in the array; you can't add
+# a single integer to itself in order to obtain the target sum.
+#
+# You can assume that there will be at most one pair of numbers summing up to the target sum.
+#
+def twoNumberSum(array, targetSum):
+	# if array == None or len(array) == 0:
+	# 	return []
+    array.sort()
+	l = 0
+	r = len(array) - 1
+	while l < r:
+		currentSum = array[l] + array[r]
+		if currentSum == targetSum:
+			return [array[l], array[r]]
+		elif currentSum > targetSum:
+			r -= 1
+		else:
+			l += 1
+	return []
